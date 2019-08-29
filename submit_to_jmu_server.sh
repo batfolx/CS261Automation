6cd5e576994d4517e753b359ce4df56d5ffa8683
VYS6 <Cosmin.Velea@gdit.com>
2019-08-29 19:05:42 -0400

    Fine working version of the submit script

--- submit_to_jmu_server.sh
+++ submit_to_jmu_server.sh
@@ -4,12 +4,7 @@
 $directory = $1
 CSProject = project0.zip
 eID = $2
-SCRIPT = "unzip /cs/home/stu/${2}/cs261/project0.zip"
 echo Attempting to see if directory exists...
-
-
-echo $1
-echo $2
 
 if [ -d $1 ]; then
 	echo Directry valid. Attempting to zip project.
@@ -17,11 +12,20 @@ if [ -d $1 ]; then
 		echo Destination address valid. Zipping project.
 		cp -rf $1 ~/zip_files//Project0
 		echo Folder copied. Zipping project.
-		zip -r project0.zip ~/zip_files/Project0
+		cd ~/zip_files/ &&  zip -r project0.zip Project0/
 	        echo Project zipped. Attempting to SSH into JMU Server.	
 		mv project0.zip ~/zip_files/
-		scp ~/zip_files/project0.zip  $2@stu.cs.jmu.edu:/cs/home/stu/${2}/cs261 
-		echo Zip file uploaded to JMU CS Server.
+		
+
+		scp ~/zip_files/project0.zip  $2@stu.cs.jmu.edu:/cs/home/stu/${2}/cs261/ 
+		if [ "$?" -eq "0" ]; then
+			echo Zip file uploaded to JMU CS Server.
+		else
+			echo SCP failed. Check directory.
+		fi
+
+		ssh $2@stu.cs.jmu.edu "ls; unzip -o  /cs/home/stu/${2}/cs261/project0.zip -d /cs/home/stu/${2}/cs261/; rm -rf /cs/home/stu/${2}/cs261/project0.zip Project0/"
+
 		
 
 
